667e656bb0f3d148f0d1d232be89798aabca86c8
Travis Cook <tdc12c@acu.edu>
2017-09-12 23:24:10 -0500

    android app1

---
+++ MyApplication/app/src/main/java/io/github/travesto/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,32 @@
+package io.github.travesto.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import java.net.URI;
+import java.net.URL;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+
+
+    }
+}
